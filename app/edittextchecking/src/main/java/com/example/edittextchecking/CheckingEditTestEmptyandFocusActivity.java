56152a8510610f834a04fc129fc41f9b13d88598
JaviBelloso <franciscojavier.belloso@bluumi.net>
2020-05-13 16:24:03 +0200

    Last commit.

---
+++ app/edittextchecking/src/main/java/com/example/edittextchecking/CheckingEditTestEmptyandFocusActivity.java
@@ -0,0 +1,104 @@
+package com.example.edittextchecking;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public abstract class CheckingEditTestEmptyandFocusActivity extends Activity {
+
+    private ViewGroup viewById;
+    private ArrayList<EditText> array;
+    private View editTextEmpty;
+
+    public View getEditTextEmpty(ArrayList<EditText> editTextsException, String messageToast) {
+        array = new ArrayList<>();
+        return StartCkecking(editTextsException,messageToast);
+    }
+
+    public void setEditTextEmpty(View editTextEmpty) {
+        this.editTextEmpty = editTextEmpty;
+    }
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+    private View StartCkecking(ArrayList<EditText> editTextsException, String messageToast) {
+        viewById = (ViewGroup) findViewById(android.R.id.content);
+        if(messageToast.equals(""))
+            messageToast="Fill this field!";
+        editTextEmpty= findAllEditTexts(viewById,editTextsException);
+        if (editTextEmpty != null) {
+            editTextEmpty.requestFocus();
+            final Drawable backgroundOriginal = editTextEmpty.getBackground();
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                editTextEmpty.setBackground(getDrawable(R.color.light_red));
+            }
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            editTextEmpty.setBackground(backgroundOriginal);
+                        }
+                    });
+
+                }
+            }, 800);
+            Toast.makeText(this, messageToast, Toast.LENGTH_SHORT).show();
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.showSoftInput(editTextEmpty, InputMethodManager.SHOW_IMPLICIT);
+        }
+        return editTextEmpty;
+    }
+
+    private View findAllEditTexts(ViewGroup viewGroup, ArrayList<EditText> editTextsException) {
+
+        View viewEmpty = null;
+        int count = viewGroup.getChildCount();
+        for (int i = 0; i < count; i++) {
+            View view = viewGroup.getChildAt(i);
+            if (view instanceof ViewGroup)
+                findAllEditTexts((ViewGroup) view, editTextsException);
+            else if (view instanceof EditText) {
+                EditText editText = (EditText) view;
+                if(editTextsException!=null){
+                    if(editText.getText().toString().equals("") && !editTextsException.contains(editText)) {
+                        array.add(editText);
+                    }
+                }else{
+                    if(editText.getText().toString().equals("")) {
+                        array.add(editText);
+                    }
+                }
+
+
+            }
+
+        }
+        if(array.size()>0)
+            return array.get(0);
+        else
+            return null;
+
+    }
+
+}
+
+
+
---
+++ app/edittextchecking/src/main/java/com/example/edittextchecking/CheckingEditTestEmptyandFocusFragment.java
@@ -0,0 +1,107 @@
+package com.example.edittextchecking;
+
+import android.annotation.SuppressLint;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+abstract class CheckingEditTestEmptyandFocusFragment extends Fragment {
+
+    private ViewGroup viewById;
+    private ArrayList<EditText> array;
+    private View editTextEmpty;
+
+
+    public View getEditTextEmpty(ArrayList<EditText> editTextsException, String messageToast) {
+        array = new ArrayList<>();
+        return StartCkecking(editTextsException,messageToast);
+    }
+
+    public void setEditTextEmpty(View editTextEmpty) {
+        this.editTextEmpty = editTextEmpty;
+    }
+
+    public CheckingEditTestEmptyandFocusFragment() {
+        // Required empty public constructor
+    }
+
+
+    @SuppressLint("ResourceType")
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View inflate = inflater.inflate(android.R.id.content, container, false);
+        viewById = (ViewGroup) inflate.findViewById(android.R.id.content);
+        return inflate;
+    }
+
+    private View StartCkecking(ArrayList<EditText> editTextsException, String messageToast) {
+        if(messageToast.equals(""))
+            messageToast="Fill this field!";
+        editTextEmpty= findAllEditTexts(viewById,editTextsException);
+        if (editTextEmpty != null) {
+            editTextEmpty.requestFocus();
+            final Drawable backgroundOriginal = editTextEmpty.getBackground();
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                editTextEmpty.setBackground(getActivity().getDrawable(R.color.light_red));
+            }
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            editTextEmpty.setBackground(backgroundOriginal);
+                        }
+                    });
+
+                }
+            }, 800);
+            Toast.makeText(getActivity(), messageToast, Toast.LENGTH_SHORT).show();
+            InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.showSoftInput(editTextEmpty, InputMethodManager.SHOW_IMPLICIT);
+        }
+        return editTextEmpty;
+    }
+
+    private View findAllEditTexts(ViewGroup viewGroup, ArrayList<EditText> editTextsException) {
+
+        View viewEmpty = null;
+        int count = viewGroup.getChildCount();
+        for (int i = 0; i < count; i++) {
+            View view = viewGroup.getChildAt(i);
+            if (view instanceof ViewGroup)
+                findAllEditTexts((ViewGroup) view, editTextsException);
+            else if (view instanceof EditText) {
+                EditText editText = (EditText) view;
+                if(editText.getText().toString().equals("") && !editTextsException.contains(editText)) {
+                    array.add(editText);
+                }
+
+            }
+
+        }
+        if(array.size()>0)
+            return array.get(0);
+        else
+            return null;
+
+    }
+}
